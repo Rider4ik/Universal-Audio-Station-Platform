cf815438813bf1d573b03fe6de64adc0f9f31281
Acer <Acer@DESKTOP-3S8TOIV>
2022-05-10 17:52:12 +0300

    Добавьте файлы проекта.

---
+++ Universal  Audio Station Platform/MainPage.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::Universal__Audio_Station_Platform::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::Universal__Audio_Station_Platform::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
